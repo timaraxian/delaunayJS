665b9ca91b342da226f41e76dc8096e903c824bd
Timothy Thatcher <thatcher.tim.tt@gmail.com>
2018-09-03 22:33:45 +0200

    :construction: wip: refactoring

---
+++ app.js
@@ -0,0 +1,31 @@
+import { NewDelaunay } from "./delaunay";
+
+const delauney = NewDelaunay()
+
+var app = new Vue({
+    el: '#app',
+
+    data: {
+        model: delauney.state
+    },
+
+    methods: {
+        addPoint: ({ clientX, clientY }) => delauney.addPoint(clientX, clientY),
+        paths: () => delauney.connectPoints(),
+    },
+
+    template: `
+<div>
+<svg @click="addPoint" width="700" height="500">
+<circle v-for="point in model.points" :cx="point.x" :cy="point.y" r="5"/>
+<polygon
+v-for="path in model.paths"
+:points="path"
+style="fill-opacity:0;stroke:rebeccapurple;stroke-width:1px"
+/>
+</svg>
+<br>
+<button @click="paths">paths</button>
+</div>
+`
+});
--- tests/delaunayTest.js
+++ tests/delaunayTest.js
@@ -8,7 +8,57 @@ describe('Delaunay', () => {
 
         expect(d.state.points.length).equals(0)
         expect(d.state.points).eql([])
-        expect(d.state.paths.length).equals(0)
-        expect(d.state.points).eql([])
+        expect(d.state.triangles.length).equals(0)
+        expect(d.state.triangles).eql([])
+    })
+
+    it('adds a point', () => {
+        const d = NewDelaunay()
+
+        const pt1 = d.addPoint(10, 25)
+        expect(d.state.points.length).equals(1)
+        expect(d.state.points).eql([pt1])
+        expect(d.state.triangles.length).equals(0)
+        expect(d.state.triangles).eql([])
+    })
+
+    it('adds multiple points', () => {
+        const d = NewDelaunay()
+
+        const pt1 = d.addPoint(10, 5)
+        const pt2 = d.addPoint(5, 10)
+        const pt3 = d.addPoint(20, 10)
+        const pt4 = d.addPoint(10, 25)
+
+        expect(d.state.points.length).equals(4)
+        expect(d.state.points).eql([pt1, pt2, pt3, pt4])
+    })
+
+    it('triangulates 3 points', () => {
+        const d = NewDelaunay()
+
+        const pt1 = d.addPoint(10, 5)
+        const pt2 = d.addPoint(5, 10)
+        const pt3 = d.addPoint(20, 10)
+
+        expect(d.state.points.length).equals(3)
+        expect(d.state.points).eql([pt1, pt2, pt3])
+
+        d.triangulate()
+        expect(d.state.triangles.length).equals(1)
+        expect(d.state.triangles).eql([[pt2, pt1, pt3]])
+    })
+
+    it('correctly triangulates 4 points', () => {
+        const d = NewDelaunay()
+
+        const pt1 = d.addPoint(10, 5)
+        const pt2 = d.addPoint(5, 10)
+        const pt3 = d.addPoint(20, 10)
+        const pt4 = d.addPoint(10, 25)
+
+        d.triangulate()
+        expect(d.state.triangles.length).equals(2)
+        expect(d.state.triangles).eql([[pt2, pt1, pt3], [pt2, pt4, pt3]])
     })
 })
